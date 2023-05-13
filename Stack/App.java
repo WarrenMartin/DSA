9802779a5cb79edf7aa59fbfcf38620bbbe44b76
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-05-13 20:24:31 +0545

    Update App.java
    Here this has implementation of both array and Linked list using stack

--- Stack/App.java
+++ Stack/App.java
@@ -1,36 +1,31 @@
 import java.util.Arrays;
+import java.util.Stack;
 
-public class App {
-
+public class Main {
 
     public static void reverseArray(int[] arr,StackIntf s){
-
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i <arr.length ; i++) {
             s.push(arr[i]);
         }
+
         int i=0;
-        while(!s.isEmpty()){
-           arr[i]=s.pop();
-           ++i;
+        while (!s.isEmpty()){
+            arr[i]=s.pop();
+            i++;
         }
 
     }
 
     public static void main(String[] args) {
-        int[] arr={1,2,3};
 
-       StackUsingArray stackObj=new StackUsingArray(arr.length);
+        int[] arr={1,2,3,4,5,6};
 
+        StackIntf stackobj=new StackUsingArray(arr.length);
+        StackIntf stackobj2=new stackUsingList();
+
+
+        reverseArray(arr,stackobj);
         System.out.println(Arrays.toString(arr));
 
-        App ab=new App();
-        ab.reverseArray(arr, stackObj);
-
-        System.out.println(Arrays.toString(arr));
-
-    
-
-    
     }
-    
 }
