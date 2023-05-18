158b0a00723d974bf3bb70262eef05e5aa6b628a
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-05-18 19:48:50 +0545

    Create Main.java

---
+++ LinkedListUsingArrays/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        sListUsingArray list=new sListUsingArray(5);
+
+        list.addAtFront(10);
+        list.addAtFront(20);
+        list.print();
+
+    }
+}
