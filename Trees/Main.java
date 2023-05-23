ff4b4c304d171c7efd1d3fd5bb2373838a108a9d
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-05-23 08:11:31 +0545

    Create Main.java

---
+++ Trees/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree t01=new BinaryTree();
+
+        t01.createTree01();
+        t01.printUsingPreOrder();
+        System.out.println("______________________");
+        t01.swap();
+        t01.printUsingPreOrder();
+//        int a=t01.count();
+//        System.out.println(a);
+
+//        t01.createTree02();
+//        t01.printUsingPreOrder();
+//        t01.swap();
+//        t01.printUsingPreOrder();
+
+
+    }
+}
